b9ec0ed3716d3473a0cb308a4de00e908022a30c
Ian <DrilonIan@yahoo.com>
2021-01-15 00:27:34 +0800

    first commit in development

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+//*components
+const Home = lazy(() => import("./page/Home"));
 
 const App: React.FC = () => {
-  return <div className="App">Hello world</div>;
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Route exact path="/" component={Home} />
+      </Suspense>
+    </Router>
+  );
 };
 
 export default App;
